d411fd430ace1480c70fb080b55a36144e13b34c
Ha-Duong Nguyen <cmpitg@gmail.com>
2026-09-03 15:28:48 +0700

    refactor: Fix indentations

--- flake.nix
+++ flake.nix
@@ -8,32 +8,32 @@
 
   outputs = inputs@{ flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
     perSystem = { pkgs, ... }: let
-        tpl-api-server = pkgs.writeShellApplication {
-          name = "api-server";
-          runtimeInputs = [ pkgs.uv python ];
-          text = ''
+      tpl-api-server = pkgs.writeShellApplication {
+        name = "api-server";
+        runtimeInputs = [ pkgs.uv python ];
+        text = ''
             exec uv run python3 api_server.py "$@"
           '';
-        };
-        app_tpl-ap-server = {
-          type = "app";
-          program = "${tpl-api-server}/bin/api-server";
-        };
-        devShell_tpl-api-server = pkgs.mkShell {
-          packages = [
-            pkgs.uv python
-          ];
-          env = {
-            UV_PYTHON = "${python}/bin/python3";
-          };
-        };
-
-        python = pkgs.python314;
-      in {
-        packages.default = tpl-api-server;
-        apps.default = app_tpl-ap-server;
-        devShells.default = devShell_tpl-api-server;
       };
+      app_tpl-ap-server = {
+        type = "app";
+        program = "${tpl-api-server}/bin/api-server";
+      };
+      devShell_tpl-api-server = pkgs.mkShell {
+        packages = [
+          pkgs.uv python
+        ];
+        env = {
+          UV_PYTHON = "${python}/bin/python3";
+        };
+      };
+
+      python = pkgs.python314;
+    in {
+      packages.default = tpl-api-server;
+      apps.default = app_tpl-ap-server;
+      devShells.default = devShell_tpl-api-server;
+    };
 
     systems = [ "x86_64-linux" "aarch64-linux" ];
   };
